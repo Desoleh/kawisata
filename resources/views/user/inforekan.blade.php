fcfd582bc5ce1daf285209d0daf3c85df697dbc4
desoleh <Muchamad.soleh@gmail.com>
2021-11-08 14:50:56 +0700

    detail profil

--- resources/views/user/inforekan.blade.php
+++ resources/views/user/inforekan.blade.php
@@ -43,14 +43,22 @@
                       <th>NIPP/NIP</th>
                       <th>Nama</th>
                       <th>Jabatan</th>
+                      <th></th>
                     </tr>
                   </thead>
                   <tbody>
                         @foreach ($employees as $employee)
                     <tr>
-                            <td><a href="{{ route('user.infoRekanDetail', $employee->nip) }}">{{ $employee->nip }}</a></td>
-                            <td>{{ $employee->nama}}</td>
-                            <td>{{ $employee->name}}</td>
+                            <td><a class="text-dark text-decoration-none" href="{{ route('user.infoRekanDetail', $employee->nip) }}">{{ $employee->nip }}</a></td>
+                            <td><a class="text-dark text-decoration-none" href="{{ route('user.infoRekanDetail', $employee->nip) }}">{{ $employee->nama}}</a></td>
+                            <td><a class="text-dark text-decoration-none" href="{{ route('user.infoRekanDetail', $employee->nip) }}">{{ $employee->name}}</a></td>
+                            <td><a class="btn btn-outline-primary text-decoration-none" href="{{ route('user.infoRekanDetail', $employee->nip) }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
+                                    </svg>
+                                </a>
+                            </td>
                     </tr>
                         @endforeach
                   </tbody>
